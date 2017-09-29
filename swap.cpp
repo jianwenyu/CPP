81a9ac8764f918f8b75cf6a8500de77f707b72fc
jianwenyu <jerryyu1202@gmail.com>
2017-09-29 13:29:04 -0700

    Draft the code frame without implementing the actually functionality of swapping two number

---
+++ swap.cpp
@@ -0,0 +1,17 @@
+#include<cstdio>
+
+using namespace std;
+
+int main(int argc,char **argv){
+    int a = 1;
+    int b = 2;
+    printf("a is %d\n",a);
+    printf("b is %d\n",b);
+    
+    //swapping code occur
+    
+    printf("a is %d\n",a);
+    printf("b is %d\n",b);
+    
+    return 0;
+}
