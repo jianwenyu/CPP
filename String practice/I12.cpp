5fce17fb65793bde2c5cc24be27d8146fdd069cd
jianwenyu <jerryyu1202@gmail.com>
2017-10-03 19:38:19 -0700

    Binary Search practise added

---
+++ String practice/I12.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+bool check_permu(string a,string b){
+    if(a.length()!=b.length()){
+        return false;
+    }else{
+        
+    }    
+}
+
+int main(int argc, char **argv){
+    
+    
+    return 0;
+}
