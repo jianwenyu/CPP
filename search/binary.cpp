1a641ed035c249659fa9967b8d048f7947e02282
jianwenyu <jerryyu1202@gmail.com>
2017-10-03 19:45:56 -0700

    update binary search

--- search/binary.cpp
+++ search/binary.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool binSearch(int min,int max,int a[],int x){
    int mid = (max+min)/2;
  
-   if(min<=max && mid<=max){
+   if(min<=max){
         if(x>a[mid]){
             return binSearch(mid+1,max,a,x);
         }else if(x<a[mid]){
@@ -21,13 +21,13 @@ bool binSearch(int min,int max,int a[],int x){
 int main(int argc,char **argv){
     int a[NUM];
     for(int i=0;i<NUM;++i){
-        a[i]=i;
+        a[i]=i+22;
     }
     for(int ii:a){
         cout<<ii<<' ';
     }
     cout<<endl;
-    int b = 31;
+    int b = 62;
     if(binSearch(0,NUM-1,a,b)){
         cout<<"Found "<<b<<" in array."<<endl;
     }else{
